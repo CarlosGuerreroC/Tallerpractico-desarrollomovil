c282e61db3c8cd6a97f122d227cca00a7db3c908
CarlosGuerreroC <cguerrerocuesta123@gmail.com>
2026-02-23 13:18:09 -0500

    Estoy agregando el ingreso y validacion de datos

---
+++ Tarea/Jueguito.js
@@ -0,0 +1,44 @@
+/*
+This function is for data entry 
+*/
+function Enterplayers(player1, player2) {
+    player1 = prompt("Player 1: Enter Name");
+    player2 = prompt("Player 2: Enter Name");
+    return { player1, player2 };
+}
+/*
+This function is to validate that the user does not 
+enter a number as a name.
+*/
+function Validate(name) {
+    const lettersonly = /^[A-Za-zÁÉÍÓÚáéíóúÑñ\s]+$/;
+    return lettersonly.test(name);
+}
+const save = Enterplayers();
+
+const validPlayer1 = Validate(save.player1);
+const validPlayer2 = Validate(save.player2);
+
+if (!validPlayer1 || !validPlayer2) {
+    alert("Numbers are not allowed as names");
+} else {
+    alert("Excellet");
+    alert(`Welcome  ${save.player1} and ${save.player2}`);
+}
+
+/*Simple Function 
+1. This attack function
+2. Each attacker has 1 attack opportunities
+*/ 
+function Attack(){}
+/*Arrow type function
+1. 
+*/
+const Cure = () =>{
+    return
+}
+/*
+1. Defend function
+*/
+const Defend = () =>{}
+
